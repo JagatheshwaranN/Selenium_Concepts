830635ee5451e4a6880f6c60d20ceb3fd97ed59d
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-03-06 20:15:17 +0530

    Selenium Webpage Actions Scenarios Updated

--- src/main/java/_01/selenium/basics/_04_WebPageActions.java
+++ src/main/java/_01/selenium/basics/_04_WebPageActions.java
@@ -2,27 +2,34 @@ package _01.selenium.basics;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class _04_WebPageActions extends _01_LaunchBrowser {
 
 	public static WebDriver _driver;
+	public static Select _selectObject;
 
 	public static void main(String[] args) {
 
 		try {
-			typeInAnElement();
-			clickOnAnElement();
+//			clearAnAnElement();
+//			clickOnAnElement();
+			selectDropDownByVisibleText();
+			//typeInAnElement();
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public static void typeInAnElement() throws InterruptedException {
+	
+	public static void clearAnAnElement() throws InterruptedException {
 		_driver = get_driver();
 		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		_driver.get("https://admin-demo.nopcommerce.com/login");
 		Thread.sleep(8000);
-		_driver.findElement(By.cssSelector("input[name='username']")).sendKeys("admin");
+		_driver.findElement(By.cssSelector("input[name='Email']")).clear();
+		Thread.sleep(3000);
 		_driver.quit();
 	}
 
@@ -35,4 +42,26 @@ public class _04_WebPageActions extends _01_LaunchBrowser {
 				.click();
 		_driver.quit();
 	}
+	
+	public static void selectDropDownByVisibleText() throws InterruptedException {
+		_driver = get_driver();
+		_driver.manage().window().maximize();
+		_driver.get("https://letcode.in/dropdowns");
+		Thread.sleep(8000);
+		_selectObject = new Select(_driver.findElement(By.cssSelector("#fruits")));
+		_selectObject.selectByVisibleText("Apple");
+		Thread.sleep(2000);
+		_driver.quit();
+	}
+
+	public static void typeInAnElement() throws InterruptedException {
+		_driver = get_driver();
+		_driver.manage().window().maximize();
+		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		Thread.sleep(8000);
+		_driver.findElement(By.cssSelector("input[name='username']")).sendKeys("admin");
+		_driver.quit();
+	}
+
+	
 }
