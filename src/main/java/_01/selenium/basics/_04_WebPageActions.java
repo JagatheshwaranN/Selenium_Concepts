b7a416e6b60401a161534513e2d3ae75e767d289
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-03-21 21:30:38 +0530

    Webpage Action Scenarios Updated

--- src/main/java/_01/selenium/basics/_04_WebPageActions.java
+++ src/main/java/_01/selenium/basics/_04_WebPageActions.java
@@ -2,6 +2,7 @@ package _01.selenium.basics;
 
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +21,7 @@ public class _04_WebPageActions extends _01_LaunchBrowser {
 	public static void main(String[] args) {
 
 		try {
-			clearAnAnElement();
+			clearAnElement();
 			clickOnAnElement();
 			dropDownOptions();
 			dropDownSelectedOption();
@@ -34,6 +35,9 @@ public class _04_WebPageActions extends _01_LaunchBrowser {
 			mouseHover();
 			mouseDragDrop();
 			mouseClickAndHold();
+			mouseRightClick();
+			mouseDoubleClick();
+			mouseClick();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -46,7 +50,7 @@ public class _04_WebPageActions extends _01_LaunchBrowser {
 		return _driver;
 	}
 
-	public static void clearAnAnElement() throws InterruptedException {
+	public static void clearAnElement() throws InterruptedException {
 		browserSetup();
 		_driver.manage().window().maximize();
 		_driver.get("https://admin-demo.nopcommerce.com/login");
@@ -193,20 +197,60 @@ public class _04_WebPageActions extends _01_LaunchBrowser {
 		_driver.manage().window().maximize();
 		_driver.get("https://www.selenium.dev/selenium/web/mouse_interaction.html");
 		_actions = new Actions(_driver);
-		_actions.dragAndDrop(_driver.findElement(By.id("draggable")), _driver.findElement(By.id("droppable"))).build().perform();
+		_actions.dragAndDrop(_driver.findElement(By.id("draggable")), _driver.findElement(By.id("droppable"))).build()
+				.perform();
 		Thread.sleep(5000);
 		_driver.findElement(By.xpath("//strong[@id='drop-status']")).isDisplayed();
 		_driver.quit();
 	}
-	
+
 	public static void mouseClickAndHold() throws InterruptedException {
 		browserSetup();
 		_driver.manage().window().maximize();
 		_driver.get("https://letcode.in/buttons");
 		_actions = new Actions(_driver);
-		_actions.clickAndHold(_driver.findElement(By.xpath("(//button[@class='button is-primary'])[2]"))).build().perform();
+		_actions.clickAndHold(_driver.findElement(By.xpath("(//button[@class='button is-primary'])[2]"))).build()
+				.perform();
 		Thread.sleep(5000);
 		_driver.findElement(By.xpath("//h2[text()='Button has been long pressed']")).isDisplayed();
+		_driver.quit();
+	}
+
+	public static void mouseRightClick() throws InterruptedException {
+		browserSetup();
+		_driver.manage().window().maximize();
+		_driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		_actions = new Actions(_driver);
+		_actions.contextClick(_driver.findElement(By.xpath("//span[contains(@class,'context-menu-one')]"))).build()
+				.perform();
+		Thread.sleep(2000);
+		_driver.findElement(By.cssSelector(".context-menu-item.context-menu-icon.context-menu-icon-copy"))
+				.isDisplayed();
+		_driver.quit();
+	}
+
+	public static void mouseDoubleClick() throws InterruptedException {
+		browserSetup();
+		_driver.manage().window().maximize();
+		_driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		_actions = new Actions(_driver);
+		_actions.doubleClick(_driver.findElement(By.xpath("//button[contains(text(),'Double-Click Me To See Alert')]")))
+				.perform();
+		Thread.sleep(2000);
+		Alert alert = _driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.dismiss();
+		_driver.quit();
+	}
+
+	public static void mouseClick() throws InterruptedException {
+		browserSetup();
+		_driver.manage().window().maximize();
+		_driver.get("https://www.selenium.dev/selenium/web/mouse_interaction.html");
+		_actions = new Actions(_driver);
+		_actions.click(_driver.findElement(By.xpath("//input[@id='clickable']"))).perform();
+		Thread.sleep(2000);
+		_driver.findElement(By.xpath("//strong[@id='click-status']")).isDisplayed();
 		_driver.quit();
 	}
 
