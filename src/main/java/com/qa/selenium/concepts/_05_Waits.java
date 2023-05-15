6715742b535e9db0e6673a9995cb5f63d9abc88c
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-15 20:05:18 +0530

    TestNG Format Conversion

--- src/main/java/com/qa/selenium/concepts/_05_Waits.java
+++ src/main/java/com/qa/selenium/concepts/_05_Waits.java
@@ -8,102 +8,106 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Sleeper;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
-public class _05_Waits extends _01_LaunchBrowser {
+public class _05_Waits {
 
-	public static WebDriver _driver;
-	public static WebDriverWait _wait;
-	public static Wait<WebDriver> _wait2;
+	private static WebDriver driver;
+	private static ChromeOptions chromeOptions;
+	private static Wait<WebDriver> wait2;
 
-	public static void main(String[] args) {
-
-		implicitWait();
-		waitUntilType1();
-		waitUntilType2();
-		waitUntilType3();
-		fluentWait();
-		fluentWait2();
-	}
-
-	public static void implicitWait() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement username = new WebDriverWait(_driver, Duration.ofSeconds(5))
+	@Test(priority = 1, enabled = true)
+	private static void implicitWait() {
+		browserSetup();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		WebElement username = new WebDriverWait(driver, Duration.ofSeconds(5))
 				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']")));
 		username.sendKeys("admin");
-		_driver.quit();
+		waitForSomeTime();
+		driver.close();
 	}
 
-	public static void waitUntilType1() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement username = new WebDriverWait(_driver, Duration.ofSeconds(5))
+	@Test(priority = 2, enabled = true)
+	private static void waitUntilType1() {
+		browserSetup();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		WebElement username = new WebDriverWait(driver, Duration.ofSeconds(5))
 				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']")));
 		username.sendKeys("admin");
-		_driver.quit();
+		driver.close();
 		/**
 		 * More Options available under ExpectedCondition Class
 		 * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html
 		 */
 	}
 
-	public static void waitUntilType2() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement loginButton = new WebDriverWait(_driver, Duration.ofSeconds(5), Duration.ofMillis(1500))
+	@Test(priority = 3, enabled = true)
+	private static void waitUntilType2() {
+		browserSetup();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		WebElement loginButton = new WebDriverWait(driver, Duration.ofSeconds(5), Duration.ofMillis(1500))
 				.until(ExpectedConditions.elementToBeClickable(
 						By.cssSelector(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button")));
 		loginButton.click();
-		_driver.quit();
+		driver.close();
 	}
 
-	public static void waitUntilType3() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement password = new WebDriverWait(_driver, Duration.ofSeconds(10), Duration.ofMillis(2000),
+	@Test(priority = 4, enabled = true)
+	private static void waitUntilType3() {
+		browserSetup();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		WebElement password = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(2000),
 				Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
 				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='password']")));
 		password.sendKeys("admin123");
-		_driver.quit();
+		driver.close();
 	}
 
-	public static void fluentWait() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		_wait2 = new FluentWait<WebDriver>(_driver).withTimeout(Duration.ofSeconds(10))
+	@Test(priority = 5, enabled = true)
+	private static void fluentWait() {
+		browserSetup();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		wait2 = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(10))
 				.pollingEvery(Duration.ofMillis(1500)).ignoring(NoSuchElementException.class)
 				.ignoring(NotFoundException.class);
-		WebElement username = _wait2.until(_driver -> _driver.findElement(By.cssSelector("input[name='username']")));
+		WebElement username = wait2.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
 		username.sendKeys("admin");
-		_driver.quit();
+		driver.close();
 	}
 
-	public static void fluentWait2() {
-
-		_driver = get_driver();
-		_driver.manage().window().maximize();
-		_driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		_wait2 = new FluentWait<WebDriver>(_driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
+	@Test(priority = 6, enabled = true)
+	private static void fluentWait2() {
+		browserSetup();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		wait2 = new FluentWait<WebDriver>(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
 				.withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofMillis(1500))
 				.ignoring(NotFoundException.class);
-		WebElement username = _wait2.until(_driver -> _driver.findElement(By.cssSelector("input[name='username']")));
+		WebElement username = wait2.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
 		username.sendKeys("admin");
-		_driver.quit();
+		driver.close();
+	}
+
+	private static WebDriver browserSetup() {
+		chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(chromeOptions);
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	private static void waitForSomeTime() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
