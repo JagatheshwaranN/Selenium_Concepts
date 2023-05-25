fdb376a53796aaa35e594fd02cf59260ce82d80a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-25 20:53:15 +0530

    WebTable Scenario Added

---
+++ src/main/java/com/qa/selenium/concepts/_18_WebTable.java
@@ -0,0 +1,51 @@
+package com.qa.selenium.concepts;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+public class _18_WebTable {
+
+	private WebDriver driver;
+
+	@Test(priority = 1, enabled = true)
+	private void getRowDataFromTable() {
+		browserSetup();
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\WebTable.html");
+		WebElement table = driver.findElement(By.id("data-table"));
+		List<WebElement> totalRows = table.findElements(By.tagName("tr"));
+		List<WebElement> totalCells = table.findElements(By.tagName("td"));
+		System.out.println("Number of table rows  => " + totalRows.size());
+		System.out.println("Number of table cells => " + totalCells.size());
+		for (WebElement row : totalRows) {
+			List<WebElement> cells = row.findElements(By.tagName("td"));
+			for (WebElement cell : cells) {
+				if (cell.getText().equalsIgnoreCase("UK")) {
+					System.out.println("The Table Row Data is ==> " + row.getText());
+				}
+			}
+		}
+		waitForSomeTime();
+		driver.close();
+	}
+
+	private WebDriver browserSetup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	private void waitForSomeTime() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
