3e11cd79c0bba3fe5f013dba3510d3a79934c185
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-06 21:11:21 +0530

    Assertion Update for Wait Scenario Inprogress

--- src/main/java/com/qa/selenium/concepts/_04_Waits.java
+++ src/main/java/com/qa/selenium/concepts/_04_Waits.java
@@ -17,6 +17,8 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class _04_Waits {
 
 	private static WebDriver driver;
@@ -27,10 +29,12 @@ public class _04_Waits {
 	private static void implicitWait() {
 		browserSetup();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement username = new WebDriverWait(driver, Duration.ofSeconds(5))
-				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']")));
-		username.sendKeys("admin");
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		WebElement input = driver.findElement(By.cssSelector("input[id='myText']"));
+		input.sendKeys("admin");
+		String result = input.getAttribute("value");
+		Assert.assertEquals(result, "admin");
 		waitForSomeTime();
 		driver.close();
 	}
@@ -38,10 +42,13 @@ public class _04_Waits {
 	@Test(priority = 2, enabled = true)
 	private static void waitUntilType1() {
 		browserSetup();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement username = new WebDriverWait(driver, Duration.ofSeconds(5))
-				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']")));
-		username.sendKeys("admin");
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		WebElement input = new WebDriverWait(driver, Duration.ofSeconds(5))
+				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[id='myText']")));
+		input.sendKeys("admin");
+		String result = input.getAttribute("value");
+		Assert.assertEquals(result, "admin");
 		waitForSomeTime();
 		driver.close();
 		/**
@@ -49,60 +56,60 @@ public class _04_Waits {
 		 * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html
 		 */
 	}
-
-	@Test(priority = 3, enabled = true)
-	private static void waitUntilType2() {
-		browserSetup();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement loginButton = new WebDriverWait(driver, Duration.ofSeconds(5), Duration.ofMillis(1500))
-				.until(ExpectedConditions.elementToBeClickable(
-						By.cssSelector(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button")));
-		loginButton.click();
-		waitForSomeTime();
-		driver.close();
-	}
-
-	@Test(priority = 4, enabled = true)
-	private static void waitUntilType3() {
-		browserSetup();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		WebElement password = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(2000),
-				Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
-				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='password']")));
-		password.sendKeys("admin123");
-		waitForSomeTime();
-		driver.close();
-	}
-
-	@Test(priority = 5, enabled = true)
-	private static void fluentWait() {
-		browserSetup();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(10))
-				.pollingEvery(Duration.ofMillis(1500)).ignoring(NoSuchElementException.class)
-				.ignoring(NotFoundException.class);
-		WebElement username = wait.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
-		username.sendKeys("admin");
-		waitForSomeTime();
-		driver.close();
-	}
-
-	@Test(priority = 6, enabled = true)
-	private static void fluentwait() {
-		browserSetup();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
-		wait = new FluentWait<WebDriver>(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
-				.withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofMillis(1500))
-				.ignoring(NotFoundException.class);
-		WebElement username = wait.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
-		username.sendKeys("admin");
-		waitForSomeTime();
-		driver.close();
-	}
+//
+//	@Test(priority = 3, enabled = true)
+//	private static void waitUntilType2() {
+//		browserSetup();
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+//		WebElement loginButton = new WebDriverWait(driver, Duration.ofSeconds(5), Duration.ofMillis(1500))
+//				.until(ExpectedConditions.elementToBeClickable(
+//						By.cssSelector(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button")));
+//		loginButton.click();
+//		waitForSomeTime();
+//		driver.close();
+//	}
+//
+//	@Test(priority = 4, enabled = true)
+//	private static void waitUntilType3() {
+//		browserSetup();
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+//		WebElement password = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(2000),
+//				Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
+//				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='password']")));
+//		password.sendKeys("admin123");
+//		waitForSomeTime();
+//		driver.close();
+//	}
+//
+//	@Test(priority = 5, enabled = true)
+//	private static void fluentWait() {
+//		browserSetup();
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+//		wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(10))
+//				.pollingEvery(Duration.ofMillis(1500)).ignoring(NoSuchElementException.class)
+//				.ignoring(NotFoundException.class);
+//		WebElement username = wait.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
+//		username.sendKeys("admin");
+//		waitForSomeTime();
+//		driver.close();
+//	}
+//
+//	@Test(priority = 6, enabled = true)
+//	private static void fluentwait() {
+//		browserSetup();
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+//		wait = new FluentWait<WebDriver>(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER)
+//				.withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofMillis(1500))
+//				.ignoring(NotFoundException.class);
+//		WebElement username = wait.until(driver -> driver.findElement(By.cssSelector("input[name='username']")));
+//		username.sendKeys("admin");
+//		waitForSomeTime();
+//		driver.close();
+//	}
 
 	private static WebDriver browserSetup() {
 		chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--remote-allow-origins=*");
+		// chromeOptions.addArguments("--remote-allow-origins=*");
 		driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		return driver;
