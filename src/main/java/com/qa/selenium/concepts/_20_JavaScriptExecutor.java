eb7a18e3db618bb962623ab50dde38a4b64f14d8
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-06 20:54:28 +0530

    JavaScript Executor Scenarios Updated

--- src/main/java/com/qa/selenium/concepts/_20_JavaScriptExecutor.java
+++ src/main/java/com/qa/selenium/concepts/_20_JavaScriptExecutor.java
@@ -13,25 +13,73 @@ public class _20_JavaScriptExecutor {
 
 	private WebDriver driver;
 	private JavascriptExecutor jsExecutor;
-	
+
 	@Test(priority = 1, enabled = true)
 	private void EnableElement() {
 		browserSetup();
-		driver.get("D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
 		WebElement input = driver.findElement(By.id("myText"));
 		input.sendKeys("Selenium");
 		driver.findElement(By.xpath("//button")).click();
 		waitForSomeTime();
 		String getTextWhenDisable = input.getText();
 		Assert.assertEquals(getTextWhenDisable, "");
-		jsExecutor = (JavascriptExecutor)driver;
+		jsExecutor = (JavascriptExecutor) driver;
 		jsExecutor.executeScript("arguments[0].removeAttribute('disabled');", input);
 		String getTextWhenEnable = input.getAttribute("value");
 		Assert.assertEquals(getTextWhenEnable, "Selenium");
 		waitForSomeTime();
 		driver.close();
 	}
+
+	@Test(priority = 2, enabled = true)
+	private void enterText() {
+		var text = "Selenium";
+		browserSetup();
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		WebElement input = driver.findElement(By.id("myText"));
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].value='" + text + "';", input);
+		String result = input.getAttribute("value");
+		Assert.assertEquals(result, "Selenium");
+		waitForSomeTime();
+		driver.close();
+	}
+
+	@Test(priority = 3, enabled = true)
+	private void clickElement() {
+		browserSetup();
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		WebElement input = driver.findElement(By.id("myText"));
+		input.sendKeys("Selenium");
+		WebElement button = driver.findElement(By.xpath("//button"));
+		jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript("arguments[0].click()", button);
+		boolean flag = input.getAttribute("disabled") != null;
+		Assert.assertEquals(flag, true);
+		waitForSomeTime();
+		driver.close();
+	}
 	
+	@Test(priority = 4, enabled = true)
+	private void clearElement() {
+		browserSetup();
+		driver.get(
+				"D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+		WebElement input = driver.findElement(By.id("myText"));
+		input.sendKeys("Selenium");
+		waitForSomeTime();
+		jsExecutor = (JavascriptExecutor)driver;
+		jsExecutor.executeScript("arguments[0].value='';", input);
+		String getTextWhenEnable = input.getAttribute("value");
+		Assert.assertEquals(getTextWhenEnable, "");
+		waitForSomeTime();
+		driver.close();
+	}
+
 	private WebDriver browserSetup() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
