cbe1c60c8fcb2c5fddcdaa7e93755e86ded7bcbe
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-20 21:47:47 +0530

    Handle Proxy Using Chrome Dev Tool

--- src/main/java/com/qa/selenium/scenarios/_03_Handle_Proxy.java
+++ src/main/java/com/qa/selenium/scenarios/_03_Handle_Proxy.java
@@ -1,8 +1,16 @@
 package com.qa.selenium.scenarios;
 
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v112.network.Network;
+import org.openqa.selenium.devtools.v112.network.model.Headers;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -16,6 +24,11 @@ public class _03_Handle_Proxy {
 
 	private static WebDriver driver;
 	private static FirefoxOptions firefoxOptions;
+	private static Map<String, Object> header;
+	private static String basicAuthentication;
+	private static DevTools devTools;
+	private static String username = "admin";
+	private static String password = "admin";
 	
 	@Test(priority = 1, enabled = true)
 	private static void handleProxyAuthenticationOnChrome() {
@@ -37,10 +50,29 @@ public class _03_Handle_Proxy {
 		driver.close();
 	}
 	
+	
 	@Test(priority = 3, enabled = true)
 	private static void handleProxyAuthenticationOnFirefox() {
 		fireFoxBrowserSetup();
 		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+		String result = driver.findElement(By.xpath("//h3[text()='Basic Auth']")).getText();
+		Assert.assertEquals(result, "Basic Auth");
+		waitForSomeTime();
+		driver.close();
+	}
+	
+	@Test(priority = 4, enabled = true)
+	private static void handleProxyUsingChromeDevTool() {
+		chromeBrowserSetup();
+		devTools = ((ChromeDriver) driver).getDevTools();
+		devTools.createSession();
+		devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+		header = new HashMap<>();
+		basicAuthentication = "Basic "
+				+ new String(Base64.getEncoder().encode(String.format("%s:%s", username, password).getBytes()));
+		header.put("Authorization", basicAuthentication);
+		devTools.send(Network.setExtraHTTPHeaders(new Headers(header)));
+		driver.get("http://the-internet.herokuapp.com/basic_auth");
 		String result = driver.findElement(By.xpath("//h3[text()='Basic Auth']")).getText();
 		Assert.assertEquals(result, "Basic Auth");
 		waitForSomeTime();
