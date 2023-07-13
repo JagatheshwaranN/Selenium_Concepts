04011e44a6dd87160cadd962bd3fecd45fd5adaa
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-07-13 21:42:06 +0530

    SeleniumConcepts - Handling PDF Files Inprogress

--- src/main/java/com/qa/selenium/scenarios/_10_Handle_PDF_Files.java
+++ src/main/java/com/qa/selenium/scenarios/_10_Handle_PDF_Files.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -19,6 +21,7 @@ public class _10_Handle_PDF_Files {
 	private WebDriver driver;
 
 	String pdfURL = "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf";
+	String pdfURL2 = "https://www.inkit.com/blog/pdf-the-best-digital-document-management";
 
 	@Test(priority = 1, enabled = false)
 	private void getPDFFilePageCount() {
@@ -29,7 +32,7 @@ public class _10_Handle_PDF_Files {
 		waitForSomeTime();
 		driver.close();
 	}
-	
+
 	@Test(priority = 2, enabled = false)
 	private void getPDFFileContent() {
 		browserSetup();
@@ -45,7 +48,7 @@ public class _10_Handle_PDF_Files {
 		waitForSomeTime();
 		driver.close();
 	}
-	
+
 	@Test(priority = 3, enabled = false)
 	private void setPageNumberAndGetPDFFileContent() {
 		browserSetup();
@@ -56,6 +59,36 @@ public class _10_Handle_PDF_Files {
 			String pdfFileContent = pdfTextStripper.getText(getPDFDocument());
 			System.out.println(pdfFileContent);
 			Assert.assertTrue(pdfFileContent.contains("Sample Files"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		waitForSomeTime();
+		driver.close();
+	}
+
+	@Test(priority = 4, enabled = true)
+	private void readPDFFileFromBrowser() {
+		browserSetup();
+		driver.get("https://www.inkit.com/blog/pdf-the-best-digital-document-management");
+		driver.findElement(By.id("ecSubmitAgreeButton")).click();
+		waitForSomeTime();
+		driver.findElement(By.xpath("//a[text()='trillions of PDFs']")).click();
+		waitForSomeTime();
+		String pdfFileURL = driver.getCurrentUrl();
+		try {
+			URL url = new URL(pdfFileURL);
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.addRequestProperty("User-Agent", "Chrome"); // Optional statement
+			InputStream ipInputStream = urlConnection.getInputStream();
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(ipInputStream);
+			PDDocument pdDocument = PDDocument.load(bufferedInputStream);
+			int pdfFilePages = pdDocument.getNumberOfPages();
+			Assert.assertEquals(pdfFilePages, 43);
+			PDFTextStripper pdfTextStripper = new PDFTextStripper();
+			String pdfFileContent = pdfTextStripper.getText(pdDocument);
+			System.out.println(pdfFileContent);
+			Assert.assertTrue(pdfFileContent.contains("PDF Days Europe 2018"));
+			Assert.assertTrue(pdfFileContent.contains("Thank you! Any questions?"));
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
