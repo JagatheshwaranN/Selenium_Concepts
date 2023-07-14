695f72b1a3e4019213dc77a678c577800e4fcbda
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-07-14 21:14:37 +0530

    SeleniumConcepts - PDF Image Handling Utility Inprogress

--- src/main/java/com/qa/selenium/scenarios/_10_Handle_PDF_Files.java
+++ src/main/java/com/qa/selenium/scenarios/_10_Handle_PDF_Files.java
@@ -1,7 +1,9 @@
 package com.qa.selenium.scenarios;
 
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -11,9 +13,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
@@ -193,6 +198,68 @@ public class _10_Handle_PDF_Files {
 			}
 		}
 		return images;
+	}
+
+	@SuppressWarnings("unused")
+	private void PDFBoxExtractImage(PDDocument document) {
+		PDPageTree pdPageTree = document.getPages();
+		for (PDPage pdPage : pdPageTree) {
+			PDResources pdResources = pdPage.getResources();
+			for (COSName cosName : pdResources.getXObjectNames()) {
+				try {
+					PDXObject pdxObject = pdResources.getXObject(cosName);
+					if (pdxObject instanceof PDImageXObject) {
+						File file = new File("./pdfImages/" + System.nanoTime() + ".png");
+						ImageIO.write(((PDImageXObject) pdxObject).getImage(), "png", file);
+					}
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private void compareImages(File file1, File file2) {
+		BufferedImage image1 = null;
+		BufferedImage image2 = null;
+		try {
+			image1 = ImageIO.read(file1);
+			image2 = ImageIO.read(file2);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		int image1Width = image1.getWidth();
+		int image2Width = image2.getWidth();
+		int image1Height = image1.getHeight();
+		int image2Height = image2.getHeight();
+		if (image1Width != image2Width && image1Height != image2Height) {
+			
+			System.out.println("Error: Image1 and Image2 dimensions are mismatch");
+		} else {
+			long imagesDifference = 0;
+			for (int yAxis = 0; yAxis < image1Height; yAxis++) {
+
+				for (int xAxis = 0; xAxis < image1Width; xAxis++) {
+
+					int image1RGBValue = image1.getRGB(xAxis, yAxis);
+					int image2RGBValue = image2.getRGB(xAxis, yAxis);
+					int image1RedValue = (image1RGBValue >> 16) & 0xff;
+					int image1GreenValue = (image1RGBValue >> 8) & 0xff;
+					int image1BlueValue = (image1RGBValue) & 0xff;
+					int image2RedValue = (image2RGBValue >> 16) & 0xff;
+					int image2GreenValue = (image2RGBValue >> 8) & 0xff;
+					int image2BlueValue = (image2RGBValue) & 0xff;
+					imagesDifference += Math.abs(image1RedValue - image2RedValue);
+					imagesDifference += Math.abs(image1GreenValue - image2GreenValue);
+					imagesDifference += Math.abs(image1BlueValue - image2BlueValue);
+				}
+			}
+			double totalImagePixels = image1Width * image1Height * 3;
+			double averageImageDifferencePixels = imagesDifference / totalImagePixels;
+			double imageDifferenceInpercentage = (averageImageDifferencePixels / 255) * 100;
+			System.out.println("Image Difference In Percentage: " + imageDifferenceInpercentage);
+		}
 	}
 
 	private WebDriver browserSetup() {
