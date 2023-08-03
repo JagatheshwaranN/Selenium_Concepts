530d49562956d50a70480d80b2932e6c371b5d51
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-03 21:35:00 +0530

    SeleniumConcepts - BIDI API scenarios updated

--- src/main/java/com/qa/selenium/concepts/_23_Bidirectional_API.java
+++ src/main/java/com/qa/selenium/concepts/_23_Bidirectional_API.java
@@ -1,22 +1,32 @@
 package com.qa.selenium.concepts;
 
+import java.io.ByteArrayInputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.NetworkInterceptor;
 import org.openqa.selenium.devtools.events.DomMutationEvent;
 import org.openqa.selenium.logging.HasLogEvents;
+import org.openqa.selenium.remote.http.HttpResponse;
+import org.openqa.selenium.remote.http.Route;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.google.common.net.MediaType;
 
 public class _23_Bidirectional_API {
 
@@ -34,8 +44,8 @@ public class _23_Bidirectional_API {
 		waitForSomeTime();
 		driver.close();
 	}
-	
-	@Test(priority = 2, enabled = true)
+
+	@Test(priority = 2, enabled = false)
 	private void getDomMutation() throws InterruptedException {
 		chromeBrowserSetup();
 		AtomicReference<DomMutationEvent> seen = new AtomicReference<>();
@@ -47,11 +57,50 @@ public class _23_Bidirectional_API {
 		chromeDriver.get("https://www.google.com");
 		WebElement span = chromeDriver.findElement(By.xpath("//span[]"));
 		((JavascriptExecutor) chromeDriver).executeScript("arguments[0].setAttribute('cheese', 'gouda');", span);
-		//Assert.assertEquals(latch.await(10, TimeUnit.SECONDS),true );
-		Assert.assertEquals(seen.get().getAttributeName(),"cheese");
+		// Assert.assertEquals(latch.await(10, TimeUnit.SECONDS),true );
+		Assert.assertEquals(seen.get().getAttributeName(), "cheese");
 		Assert.assertEquals(seen.get().getCurrentValue(), "gouda");
 		waitForSomeTime();
 		driver.close();
+	}
+
+	@Test(priority = 3, enabled = false)
+	private void handleJSException() {
+		chromeBrowserSetup();
+		DevTools devTools = chromeDriver.getDevTools();
+		devTools.createSessionIfThereIsNotOne();
+		List<JavascriptException> jsExceptions = new ArrayList<>();
+		Consumer<JavascriptException> addEntry = jsExceptions::add;
+		devTools.getDomains().events().addJavascriptExceptionListener(addEntry);
+		chromeDriver.get(
+				"file:///D:/Environment_Collection/Eclipse_Env/Workspace/Selenium_Concepts/src/main/resources/supportFiles/WebTable.html");
+		WebElement link = chromeDriver.findElement(By.xpath("//div//a[text()='Canada']"));
+		((JavascriptExecutor) chromeDriver).executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);",
+				link, "onclick", "throw new Error('JavaScriptException Demo!')");
+		link.click();
+		for (JavascriptException jse : jsExceptions) {
+			System.out.println("JS Exception Message : " + jse.getMessage());
+			System.out.println("JS Exception SysInfo : " + jse.getSystemInformation());
+			jse.printStackTrace();
+		}
+		waitForSomeTime();
+		chromeDriver.close();
+	}
+
+	@Test(priority = 4, enabled = true)
+	private void handleNetworkIntercept() {
+		chromeBrowserSetup();
+		try (NetworkInterceptor interceptor = new NetworkInterceptor(chromeDriver,
+				Route.matching(req -> true)
+						.to(() -> req -> new HttpResponse().setStatus(200)
+								.addHeader("Content-Type", MediaType.HTML_UTF_8.toString())
+								.setContent(() -> new ByteArrayInputStream("Hello World".getBytes()))))) {
+			chromeDriver.get("https://example.com/");
+			String source = chromeDriver.getPageSource();
+			Assert.assertTrue(source.contains("Hello World"));
+		}
+		waitForSomeTime();
+		chromeDriver.close();
 	}
 
 	private WebDriver browserSetup() {
@@ -59,7 +108,7 @@ public class _23_Bidirectional_API {
 		driver.manage().window().maximize();
 		return driver;
 	}
-	
+
 	private WebDriver chromeBrowserSetup() {
 		chromeDriver = new ChromeDriver();
 		chromeDriver.manage().window().maximize();
