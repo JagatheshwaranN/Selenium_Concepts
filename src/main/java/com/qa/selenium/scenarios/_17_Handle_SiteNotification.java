87cf37f9f5d81ff20920437c144f61fbdf11d1c6
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-20 18:52:06 +0530

    Selenium Browser Launch Issue Fix

--- src/main/java/com/qa/selenium/scenarios/_17_Handle_SiteNotification.java
+++ src/main/java/com/qa/selenium/scenarios/_17_Handle_SiteNotification.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,16 +15,15 @@ import junit.framework.Assert;
 
 public class _17_Handle_SiteNotification {
 
-	private WebDriver driver;
-	private ChromeOptions chromeOptions;
-	
 	@Test(priority = 1, enabled = true)
 	private void disableSiteNotification() {
-		chromeOptions = new ChromeOptions();
+
+		System.setProperty("webdriver.http.factory", "jdk-http-client");
+		ChromeOptions chromeOptions = new ChromeOptions();
 		Map<String, Object> preference = new HashMap<String, Object>();
 		preference.put("profile.default_content_setting_values.notifications", 2);
 		chromeOptions.setExperimentalOption("prefs", preference);
-		driver = new ChromeDriver(chromeOptions);
+		WebDriver driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 		Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
