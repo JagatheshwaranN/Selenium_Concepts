dd1ddda595dc2e3d41d91c7300d84ca0c3393258
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-12 12:28:11 +0530

    Selenium Concepts Folder Organize - Locators Updated

--- src/main/java/concepts/locators/RelativeLocatorsTestCase.java
+++ src/main/java/concepts/locators/RelativeLocatorsTestCase.java
@@ -2,6 +2,8 @@ package concepts.locators;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,12 +11,17 @@ import org.testng.annotations.Test;
 
 import scenarios.DriverConfiguration;
 
+import java.time.Duration;
+
 import static org.openqa.selenium.support.locators.RelativeLocator.*;
 
 public class RelativeLocatorsTestCase {
 
 	// Declare a WebDriver instance to interact with the web browser.
 	private WebDriver driver;
+
+	// Declare a WebDriverWait instance to be used for explicit waits
+	private WebDriverWait wait;
 
 	@BeforeMethod
 	public void setUp() {
@@ -33,36 +40,82 @@ public class RelativeLocatorsTestCase {
 
 	@Test(priority = 1)
 	public void getBelowElement() {
+		// Navigate to the specified URL
 		driver.get("https://automationbookstore.dev/");
+
+		// Wait for the page to fully load before interacting with elements
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+		// Find the element below the element with id "pid1" using RelativeLocator
 		String bookId = driver.findElement(with(By.tagName("li")).below(By.id("pid1"))).getAttribute("id");
+
+		// Assert that the retrieved element's ID matches the expected value "pid5"
 		Assert.assertEquals(bookId, "pid5");
 	}
 
 	@Test(priority = 2)
 	public void getAboveElement() {
+		// Navigate to the specified URL
 		driver.get("https://automationbookstore.dev/");
+
+		// Wait for the page to fully load before interacting with elements
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+		// Find the element above the element with id "pid8" using RelativeLocator
 		String bookId = driver.findElement(with(By.tagName("li")).above(By.id("pid8"))).getAttribute("id");
+
+		// Assert that the retrieved element's ID matches the expected value "pid4"
 		Assert.assertEquals(bookId, "pid4");
 	}
 
+
 	@Test(priority = 3)
 	public void getToLeftOfElement() {
+		// Navigate to the specified URL
 		driver.get("https://automationbookstore.dev/");
+
+		// Wait for the page to fully load before interacting with elements
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+		// Find the element to the left of the element with id "pid2" using RelativeLocator
 		String bookId = driver.findElement(with(By.tagName("li")).toLeftOf(By.id("pid2"))).getAttribute("id");
+
+		// Assert that the retrieved element's ID matches the expected value "pid1"
 		Assert.assertEquals(bookId, "pid1");
 	}
 
 	@Test(priority = 4)
 	public void getToRightOfElement() {
+		// Navigate to the specified URL
 		driver.get("https://automationbookstore.dev/");
+
+		// Wait for the page to fully load before interacting with elements
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+		// Find the element to the right of the element with id "pid5" using RelativeLocator
 		String bookId = driver.findElement(with(By.tagName("li")).toRightOf(By.id("pid5"))).getAttribute("id");
+
+		// Assert that the retrieved element's ID matches the expected value "pid6"
 		Assert.assertEquals(bookId, "pid6");
 	}
 
 	@Test(priority = 5)
 	public void getNearElement() {
+		// Navigate to the specified URL
 		driver.get("https://www.google.com/");
+
+		// Wait for the page to fully load before interacting with elements
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+		// Find the input element near the element with the name "btnI" using RelativeLocator
 		String buttonValue = driver.findElement(with(By.tagName("input")).near(By.name("btnI"))).getAttribute("value");
+
+		// Assert that the retrieved element's value matches the expected value "Google Search"
 		Assert.assertEquals(buttonValue, "Google Search");
 	}
 
--- src/main/java/concepts/locators/TraditionalLocatorTypesTestCase.java
+++ src/main/java/concepts/locators/TraditionalLocatorTypesTestCase.java
@@ -30,26 +30,46 @@ public class TraditionalLocatorTypesTestCase {
 	}
 
 	@Test(priority = 1)
-    public void traditionalLocators() {
-        driver.get("https://www.selenium.dev/selenium/web/inputs.html");
-        // 1. Name
-        Assert.assertTrue(driver.findElement(By.name("no_type")).isDisplayed());
-        // 2. XPath
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@type='checkbox']")).isDisplayed());
+	public void traditionalLocators() {
 
-        driver.navigate().to("https://www.selenium.dev/selenium/web/mouse_interaction.html");
-        // 3. Id
-        Assert.assertTrue(driver.findElement(By.id("click")).isDisplayed());
-        // 4. LinkText
-        Assert.assertTrue(driver.findElement(By.linkText("Click for Results Page")).isDisplayed());
-        // 5. PartialLinkText
-        Assert.assertTrue(driver.findElement(By.partialLinkText("Click for Result")).isDisplayed());
-        // 6. Class
-        Assert.assertTrue(driver.findElement(By.className("ui-droppable")).isDisplayed());
-        // 7. CSS
-        Assert.assertTrue(driver.findElement(By.cssSelector(".ui-widget-header.ui-droppable")).isDisplayed());
-        // 8. Tag
-        Assert.assertTrue(driver.findElement(By.tagName("a")).isDisplayed());
-    }
+		// 1. Name
+		// Selects the input element with the name `no_type`.
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		Assert.assertTrue(driver.findElement(By.name("no_type")).isDisplayed());
+
+		// 2. XPath
+		// Selects the input element whose type attribute is `checkbox`.
+		driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+		Assert.assertTrue(driver.findElement(By.xpath("//input[@type='checkbox']")).isDisplayed());
+
+		// 3. Id
+		// Select the element with the ID `click`.
+		driver.navigate().to("https://www.selenium.dev/selenium/web/mouse_interaction.html");
+		Assert.assertTrue(driver.findElement(By.id("click")).isDisplayed());
+
+		// 4. LinkText
+		// Selects the anchor element whose link text is `Click for Results Page`.
+		driver.navigate().to("https://www.selenium.dev/selenium/web/mouse_interaction.html");
+		Assert.assertTrue(driver.findElement(By.linkText("Click for Results Page")).isDisplayed());
+
+		// 5. PartialLinkText
+		// Selects the anchor element whose link text contains the text `Click for Result`.
+		driver.navigate().to("https://www.selenium.dev/selenium/web/mouse_interaction.html");
+		Assert.assertTrue(driver.findElement(By.partialLinkText("Click for Result")).isDisplayed());
+
+		// 6. Class
+		// Selects the element with the class `ui-droppable`.
+		driver.get("https://www.selenium.dev/selenium/web/droppable.html");
+		Assert.assertTrue(driver.findElement(By.className("ui-droppable")).isDisplayed());
+
+		// 7. CSS
+		// Selects the element with the class `ui-widget-header` and the class `ui-droppable`.
+		driver.get("https://www.selenium.dev/selenium/web/droppable.html");
+		Assert.assertTrue(driver.findElement(By.cssSelector(".ui-widget-header.ui-droppable")).isDisplayed());
+
+		// 8. Tag
+		// Selects the first anchor element on the page.
+		Assert.assertTrue(driver.findElement(By.tagName("a")).isDisplayed());
+	}
 
 }
