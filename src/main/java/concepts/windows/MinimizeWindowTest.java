b2f0e941377ff2c861b5f0a9e4895503accd02f3
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-23 09:03:26 +0530

    Selenium Scenarios Folder Revamp - FullScreen and Minimize Screen Scenarios updated

--- src/main/java/concepts/windows/MinimizeWindowTest.java
+++ src/main/java/concepts/windows/MinimizeWindowTest.java
@@ -1,11 +1,6 @@
 package concepts.windows;
 
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v119.browser.model.WindowState;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
