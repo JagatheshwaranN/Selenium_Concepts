0a2fed879ef19cd8e5372403e0e42e368d0f891c
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-24 09:27:21 +0530

    Selenium Scenarios Folder Revamp - JavaScript Scenarios Update In-progress

---
+++ src/main/java/concepts/jsexecutor/EnableElementTest.java
@@ -0,0 +1,68 @@
+package concepts.jsexecutor;
+
+import junit.framework.Assert;
+import org.openqa.selenium.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import scenarios.DriverConfiguration;
+
+public class EnableElementTest {
+
+	// Declare a WebDriver instance to interact with the web browser.
+	private WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		// Set up the WebDriver instance by calling a method named 'browserSetup' from the 'DriverConfiguration' class
+		driver = DriverConfiguration.browserSetup();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		// Check if the 'driver' variable is not null, indicating that a WebDriver instance exists.
+		if (driver != null) {
+			// If a WebDriver instance exists, quit/close the browser session.
+			driver.quit();
+		}
+	}
+
+	@Test(priority = 1)
+	public void testEnableElement() {
+		// Initialize variables for test input and expected values
+		String value = "Selenium";
+		String expectedValue1 = ""; // Empty string for disabled state
+		String expectedValue2 = "Selenium"; // Value for enabled state
+
+		// Load the HTML page containing the disabled element
+		driver.get("D:\\Environment_Collection\\Eclipse_Env\\Workspace\\Selenium_Concepts\\src\\main\\resources\\supportFiles\\DisabledElement.html");
+
+		// Locate the input element with ID "myText"
+		WebElement input = driver.findElement(By.id("myText"));
+
+		// Enter the text "Selenium" into the input field
+		input.sendKeys(value);
+
+		// Click the button using XPath selector to trigger an action
+		driver.findElement(By.xpath("//button")).click();
+
+		// Retrieve the text from the input field and store it in 'actualValue1'
+		String actualValue1 = input.getText();
+
+		// Verify that the element remains disabled after the button clicks
+		Assert.assertEquals(actualValue1, expectedValue1);
+
+		// Create a JavaScriptExecutor object to execute JavaScript scripts
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+		// Execute a JavaScript script to remove the 'disabled' attribute from the input element, enabling it
+		jsExecutor.executeScript("arguments[0].removeAttribute('disabled');", input);
+
+		// Retrieve the value of the input field and store it in 'actualValue2'
+		String actualValue2 = input.getAttribute("value");
+
+		// Verify that the element is now enabled and contains the expected value "Selenium"
+		Assert.assertEquals(actualValue2, expectedValue2);
+	}
+
+}
