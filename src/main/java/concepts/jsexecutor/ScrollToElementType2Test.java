c48cfc6d42529cc83b0802754c0bd1e21a69e1a0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-24 17:29:58 +0530

    Selenium Scenarios Folder Revamp - JavaScript Scenarios Update In-progress

--- src/main/java/concepts/jsexecutor/ScrollToElementType2Test.java
+++ src/main/java/concepts/jsexecutor/ScrollToElementType2Test.java
@@ -1,10 +1,10 @@
 package concepts.jsexecutor;
 
-import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
