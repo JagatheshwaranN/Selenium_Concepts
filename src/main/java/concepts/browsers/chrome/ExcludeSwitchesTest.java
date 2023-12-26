799ce813ff6f9e067d705713f28bb78590ac5e58
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-12-26 12:17:46 +0530

    Browser - Chrome - Chrome Browser Specific Scenarios Update - In-progress

--- src/main/java/concepts/browsers/chrome/ExcludeSwitchesTest.java
+++ src/main/java/concepts/browsers/chrome/ExcludeSwitchesTest.java
@@ -2,34 +2,28 @@ package concepts.browsers.chrome;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.manager.SeleniumManagerOutput;
-import org.openqa.selenium.remote.service.DriverFinder;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.io.File;
+import java.util.List;
 
-public class StartBrowserFromSpecifiedLocationTest {
+public class ExcludeSwitchesTest {
 
     // Declare a WebDriver instance to interact with the web browser.
     private WebDriver driver;
 
-    // Declare a ChromeOptions object to store browser configuration settings
-    ChromeOptions chromeOptions;
-
     @Test(priority = 1)
-    public void testStartBrowserFromSpecifiedLocation() {
+    public void testExcludeSwitches() {
         // Set the system property for the WebDriver to use the JDK HTTP client
         System.setProperty("webdriver.http.factory", "jdk-http-client");
 
         // Instantiate ChromeOptions to configure the ChromeDriver
-        chromeOptions = new ChromeOptions();
+        ChromeOptions chromeOptions = new ChromeOptions();
 
-        // Set the path to the Chrome browser binary
-        chromeOptions.setBinary(getChromeLocation());
+        // Add the experimental option to launch the Chrome browser by disabling the popup blocking
+        chromeOptions.setExperimentalOption("excludeSwitches", List.of("disable-popup-blocking"));
 
         // Initialize the ChromeDriver with the configured options
         driver = new ChromeDriver(chromeOptions);
@@ -51,21 +45,6 @@ public class StartBrowserFromSpecifiedLocationTest {
             // If a WebDriver instance exists, quit/close the browser session.
             driver.quit();
         }
-    }
-
-    // Method to get a chrome binary path for ChromeOptions
-    private File getChromeLocation(){
-        // Create a ChromeOptions instance
-        chromeOptions = new ChromeOptions();
-
-        // Set a browser version (optional)
-        chromeOptions.setBrowserVersion("stable");
-
-        // Get the browser path using DriverFinder utility method
-        SeleniumManagerOutput.Result location = DriverFinder.getPath(ChromeDriverService.createDefaultService(), chromeOptions);
-
-        // Extract and return the browser path from the result
-        return new File(location.getBrowserPath());
     }
 
 }
---
+++ src/main/java/concepts/browsers/chrome/logs/LogFileOutputTest.java
@@ -0,0 +1,64 @@
+package concepts.browsers.chrome.logs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.logging.Level;
+
+public class LogFileOutputTest {
+
+    // Declare a WebDriver instance to interact with the web browser
+    private WebDriver driver;
+
+    // Declare a ChromeOptions object to store browser configuration settings
+    ChromeOptions chromeOptions;
+
+    @BeforeMethod
+    public void setUp() {
+        // Set the system property for the WebDriver to use the JDK HTTP client
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        // Create a ChromeOptions instance
+        chromeOptions = new ChromeOptions();
+
+        LoggingPreferences loggingPreferences = new LoggingPreferences();
+        loggingPreferences.enable(LogType.PERFORMANCE, Level.ALL);
+        chromeOptions.setCapability(ChromeOptions.LOGGING_PREFS, loggingPreferences);
+
+        // Initialize the ChromeDriver with the configured options
+        driver = new ChromeDriver(chromeOptions);
+
+        // Maximize the browser window using WebDriver's manage() method
+        driver.manage().window().maximize();
+    }
+
+    @Test(priority = 1)
+    public void testLogPreference() {
+        // Navigate to the Google Home page.
+        driver.get("https://www.google.com/");
+
+        // Assert that the page title is "Google".
+        Assert.assertEquals(driver.getTitle(), "Google");
+
+        LogEntries logEntries = driver.manage().logs().get(LogType.PERFORMANCE);
+        Assert.assertFalse(logEntries.getAll().isEmpty());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Check if the 'driver' variable is not null, indicating that a WebDriver instance exists.
+        if (driver != null) {
+            // If a WebDriver instance exists, quit/close the browser session.
+            driver.quit();
+        }
+    }
+
+}
