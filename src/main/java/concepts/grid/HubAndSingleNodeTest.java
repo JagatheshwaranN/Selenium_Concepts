ea2e376692cfc43ee928618f2a6ef6501e138189
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-01-01 12:28:49 +0530

    Selenium Grid - Documentation In-progress and Hub With Single Node Scenario Updated

---
+++ src/main/java/concepts/grid/HubAndSingleNodeTest.java
@@ -0,0 +1,46 @@
+package concepts.grid;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class HubAndSingleNodeTest {
+
+    public WebDriver driver;
+
+    public DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    @Test
+    public void testHubAndSingleNode() {
+        String browser = "chrome";
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        capabilities.setPlatform(Platform.ANY);
+        capabilities.setBrowserName(browser);
+        ChromeOptions options = new ChromeOptions();
+        options.merge(capabilities);
+        try {
+            driver = new RemoteWebDriver(URI.create("http://192.168.1.5:4444").toURL(), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver.get("https://www.google.com/");
+        System.out.println("Title of Page " + driver.getTitle() + " from Browser " + browser);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Check if the 'driver' variable is not null, indicating that a WebDriver instance exists.
+        if (driver != null) {
+            // If a WebDriver instance exists, quit/close the browser session.
+            driver.quit();
+        }
+    }
+
+}
